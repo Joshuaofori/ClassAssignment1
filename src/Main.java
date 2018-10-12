5b579df30dbda1cd28625d9912e38534a22a2436
Joshuaofori <37700487+Joshuaofori@users.noreply.github.com>
2018-10-12 23:20:32 +0000

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    //name Joshua Ofori
+    //index number 1660217
+    public static void main(String[] args) {
+      Input input=new Input();
+
+    }
+}
